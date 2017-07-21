1baaf746144b31e799a3faadb979488b836a1227
Tobias <thd@godaddy.com>
2017-07-21 11:20:11 +0100

    Configure git status in theme

--- init.fish
+++ init.fish
@@ -1,46 +1,39 @@
-# PATH
-set -gx NVM_DIR ~/.nvm
-
-# Fish git prompt
-set __fish_git_prompt_show_informative_status 'yes'
-set __fish_git_prompt_showcolorhints 'yes'
-set __fish_git_prompt_showdirtystate 'yes'
-set __fish_git_prompt_showstashstate 'yes'
-set __fish_git_prompt_showuntrackedfiles 'yes'
-set __fish_git_prompt_showupstream 'yes'
-set __fish_git_prompt_color_branch -o white
-set __fish_git_prompt_color_untrackedfiles -o yellow
-set __fish_git_prompt_color_prefix -o blue
-set __fish_git_prompt_color_suffix -o blue
-set __fish_git_prompt_color_merging -o yellow
-set __fish_git_prompt_color_cleanstate -o green
-set __fish_git_prompt_color_dirtystate -o yellow
-set __fish_git_prompt_color_invalidstate -o red
-set __fish_git_prompt_color_upstream_ahead -o purple
-set __fish_git_prompt_color_upstream_behind -o orange
+# # PATH
+#
+# # Fish git prompt
+set -g __fish_git_prompt_show_informative_status 1
+set -g __fish_git_prompt_showcolorhints 1
+set -g __fish_git_prompt_showdirtystate 1
+set -g __fish_git_prompt_showstashstate 1
+set -g __fish_git_prompt_showuntrackedfiles 1
+set -g __fish_git_prompt_showupstream 1
+set -g __fish_git_prompt_color_branch -o white
+set -g __fish_git_prompt_color_untrackedfiles -o yellow
+set -g __fish_git_prompt_color_prefix -o blue
+set -g __fish_git_prompt_color_suffix -o blue
+set -g __fish_git_prompt_color_merging -o yellow
+set -g __fish_git_prompt_color_cleanstate -o green
+set -g __fish_git_prompt_color_dirtystate -o yellow
+set -g __fish_git_prompt_color_invalidstate -o red
+set -g __fish_git_prompt_color_upstream_ahead -o purple
+set -g __fish_git_prompt_color_upstream_behind -o orange
 
 # Status Chars
-#set __fish_git_prompt_char_dirtystate '⚡'
-#set __fish_git_prompt_char_stagedstate '→'
-#set __fish_git_prompt_char_untrackedfiles '☡'
-#set __fish_git_prompt_char_stashstate '↩'
-#set __fish_git_prompt_char_upstream_ahead '+'
-#set __fish_git_prompt_char_upstream_behind '-'
+# set -g __fish_git_prompt_char_dirtystate '⚡'
+# set -g __fish_git_prompt_char_stagedstate '→'
+# set -g __fish_git_prompt_char_untrackedfiles '!'
+# set -g __fish_git_prompt_char_stashstate '↩'
+# set -g __fish_git_prompt_char_upstream_ahead '+'
+# set -g __fish_git_prompt_char_upstream_behind '-'
 
 
 function fish_prompt
   set last_status $status
-  set_color -o red
-  printf '➜ '
-  set_color -o $fish_color_cwd
+  set_color -o purple
+  printf '» '
+  set_color -o brcyan
   printf '%s' (basename (prompt_pwd))
   set_color normal
   printf '%s ' (__fish_git_prompt)
   set_color normal
-end
-
-# nvm fish wrapper
-# uses https://github.com/edc/bass
-function nvm
-  bass source ~/.nvm/nvm.sh ';' nvm $argv
 end
